a77943f13794384c454bb020ff305bb2cbc9db1e
Sergei Durkin <durkin.sergei.dev@gmail.com>
2025-09-26 15:41:44 +0300

    feat: add isb memory barrier

--- cnt.s
+++ cnt.s
@@ -13,3 +13,8 @@ TEXT ·getCntfrq(SB), NOSPLIT, $0
     MRS     CNTFRQ_EL0, R0
     MOVD    R0,         ret+0(FP)
     RET
+
+// func isb()
+TEXT ·isb(SB), NOSPLIT, $0-0
+    WORD $0xD5033FDF // ISB SY instruction (ARM64)
+    RET
